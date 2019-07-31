6319b67901f440548e51436081d266bb4723b9ed
Michael DeLally <thoughtreactor@gmail.com>
2019-07-31 16:54:13 -0400

    Calculator UI and list mngmt logic

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Subset Sums Calculator</h1>
+    <hr>
+    <calculator />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
+
+import Calculator from './components/Calculator';
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    Calculator
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 1em;
+  margin: auto;
+  max-width: 950px;
 }
 </style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,107 @@
+<template>
+    <div>
+
+        <div class="form-group">
+            <label for="sum">Target Sum</label>
+            <input type="number"
+                   class="form-control"
+                   v-model.number="sum"
+                   id="sum"
+            >
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Set to Calculate</h5>
+                <ul>
+                    <li v-for="(n, index) in set"
+                        class="d-flex justify-content-between"
+                    >
+                        <span>{{ n }}</span>
+                        <button class="btn btn-danger btn-sm"
+                                @click.prevent="deleteNumFromSet(index)"
+                        >
+                            DELETE
+                        </button>
+                    </li>
+                </ul>
+
+                <div class="form-inline d-flex justify-content-end">
+                    <div class="form-group">
+                        <label for="addToSet">Add to Set</label>
+                        <input type="number"
+                               class="form-control"
+                               v-model.number="newNum"
+                               id="addToSet"
+                        >
+                    </div>
+                    <button class="add-btn btn btn-primary btn-md"
+                            @click.prevent="addNumToSet"
+                    >
+                        ADD
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-end m-4">
+            <button class="btn btn-success btn-lg">
+                Calculate!
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'calculator',
+    data() {
+      return {
+        set: [5,15,1,3,8,7,15],
+        sum: 30,
+        newNum: 1
+      }
+    },
+
+    computed: {
+      results() {
+
+      }
+    },
+
+    methods: {
+      addNumToSet() {
+        this.set.push(newNum);
+        this.newNum = 1;
+      },
+      deleteNumFromSet(index) {
+        this.set.splice(index, 1);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    label {
+        font-weight: 700;
+    }
+
+    div.form-inline > div.form-group label {
+        margin-right: 6px;
+    }
+
+    button.add-btn {
+        margin-left: 6px;
+    }
+
+    li {
+        margin-bottom: 8px;
+        background-color: #e6e6e6;
+        padding: 2px 6px;
+        border-radius: 3px;
+    }
+
+    li > span {
+        font-weight: 700;
+    }
+</style>
